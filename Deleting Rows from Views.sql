a00e51660d5f67b992152ae6ed6a4ab81e2b0513
Rajat Prakash <rajatprakash904@gmail.com>
2020-04-16 19:09:01 +0530

    Deleting rows from tables using view aka deleting rows from view

---
+++ Deleting Rows from Views.sql
@@ -0,0 +1,38 @@
+use [Rajat 720_140]
+go
+
+begin transaction
+
+select count(*) from view_department
+
+delete from view_department 
+where Amount = 999.99
+
+select count(*) from view_department
+
+Rollback Transaction
+
+/* 
+Output:
+view_department' is not updatable because the modification affects multiple base tables.
+
+Multiple tables are there hence cannot delete directly ,
+But for a simple view showing data of only one table delete operation can be performed
+*/
+go
+
+
+create view simple as 
+select * from tblTransaction 
+go
+
+begin transaction
+
+select count(*) from simple
+
+delete from simple
+where EmployeeID = 502
+
+select count(*) from simple
+
+rollback transaction
