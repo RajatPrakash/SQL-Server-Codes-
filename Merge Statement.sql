8e4f9925e25ea07546ee438954d94ccac9be7a75
Rajat Prakash <rajatprakash904@gmail.com>
2020-04-19 18:22:42 +0530

    Modifed Merge statement With more advance query

--- Merge Statement.sql
+++ Merge Statement.sql
@@ -31,6 +31,34 @@ Refine the ON clause to ensure a target row matches at most one source row, or u
 go
 ---finding duplicates on saource table
 
-select EmployeeID,DateOfTransaction,count(*) from newtransaction
+select EmployeeID,DateOfTransaction,sum(Amount) from newtransaction
 group by EmployeeID,DateOfTransaction
-having count(*) > 1
+--having count(*) > 1
+--order by count desc 
+
+select count(*) from newtransaction --where EmployeeID = 815
+
+------ implementing the select statement, FINAL MERGE STATEMENT ----------
+
+BEGIN TRANSACTION
+
+
+MERGE into tblTransaction as T   --T : traget
+using (select EmployeeID,DateOfTransaction,sum(Amount) as Amount from newtransaction
+group by EmployeeID,DateOfTransaction) as S	     --S : Source
+ON T.Employeeid = S.Employeeid AND
+T.DateOfTransaction = S.DateOfTransaction
+WHEN MATCHED THEN
+	UPDATE SET Amount = T.Amount + S.Amount 
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT ([Amount],[DateOfTransaction],[EmployeeID])
+	values(s.Amount,s.DateOfTransaction,S.Employeeid)
+	--outptut : 1667 rows affected	but we neww to check the output what is affected so
+	
+	output inserted.*,deleted.*
+	;  
+
+
+ROLLBACK TRANSACTION
+
+Disable trigger [TR_tblTransaction] on tblTransaction
