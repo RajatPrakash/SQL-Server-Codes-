e3a86dfe997a2b2f6cbd6a39de0c8f410cc5e863
Rajat Prakash <rajatprakash904@gmail.com>
2020-04-25 12:55:25 +0530

    Working with over funtion along with partition by and on top of that using order by , all within over()

--- OVER.sql
+++ OVER.sql
@@ -5,3 +5,55 @@ select a.EmployeeID,CONCAT(e.EmployeeFirstName,' ',e.EmployeeLastName) as FULLNA
 on
 e.EmployeeID = a.EmployeeID
 order by e.Department
+
+
+select e.EmployeeID,year(a.AttendanceMonth) as Year,sum(a.NumberAttendance) as attendance from employee e join attendance a
+on 
+e.EmployeeID = a.EmployeeID
+group by e.EmployeeID,a.AttendanceMonth
+order by e.EmployeeID,Year
+
+
+-------------------over -----------------------------
+
+select e.EmployeeID,a.AttendanceMonth,a.NumberAttendance,
+sum(a.NumberAttendance) over()  as total  --35409 total sum(a.NumberAttendance)
+from employee e join Attendance a
+on a.EmployeeID = e.EmployeeID
+where a.AttendanceMonth < '20150101'
+
+
+
+
+-----------------over partition by --------------------------
+--partition define the range from where to where it has to work
+
+select e.EmployeeID,a.AttendanceMonth,a.NumberAttendance,
+sum(a.NumberAttendance) over(partition by e.employeeID)  as total  --sum(a.NumberAttendance) employeewise .
+from employee e join Attendance a
+on a.EmployeeID = e.EmployeeID
+where a.AttendanceMonth < '20150101'
+-- group by e.EmployeeID,a.AttendanceMonth
+
+
+
+
+
+--------OVER Partition By along with order by---------------------------
+--------this will give the running total in orderly fashion
+
+
+select e.EmployeeID,a.AttendanceMonth,a.NumberAttendance,
+sum(a.NumberAttendance) over(partition by a.employeeid,year(a.AttendanceMonth) order by a.AttendanceMonth) as total
+from employee e join attendance a
+on e.EmployeeID = a.EmployeeID
+--where a.AttendanceMonth < '20150101'
+--order by e.EmployeeID,a.AttendanceMonth
+
+
+
+
+
+
+
+
