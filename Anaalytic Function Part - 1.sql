5e9008e996d0c46ae13ea492b2eb7c1d9188985a
Rajat Prakash <rajatprakash904@gmail.com>
2020-04-27 16:13:34 +0530

    Analytic fucntion LAG and LEAD

--- Anaalytic Function Part - 1.sql
+++ Anaalytic Function Part - 1.sql
@@ -8,6 +8,23 @@ go
 select a.EmployeeID,a.AttendanceMonth,a.NumberAttendance,
 first_value(a.NumberAttendance) 
 over(partition by a.employeeID order by a.AttendanceMonth
-rows between 2 preceding and current row ) as lastmonth
+rows between 2 preceding and current row) as lastmonth
 from Attendance a join EMPLOYEE e 
 on a.EmployeeID = e.EmployeeID
+
+
+-- last value 
+select a.EmployeeID,a.AttendanceMonth,a.NumberAttendance,
+last_value(a.NumberAttendance) 
+over(partition by a.employeeID order by a.AttendanceMonth
+rows between 2 preceding and current row) as lastmonth
+from Attendance a join EMPLOYEE e 
+on a.EmployeeID = e.EmployeeID
+
+
+select a.EmployeeID,a.AttendanceMonth,a.NumberAttendance,
+last_value(a.NumberAttendance) 
+over(partition by a.employeeID order by a.AttendanceMonth) as lastmonth -- with default range value unbounded preceding and current row
+from Attendance a join EMPLOYEE e 
+on a.EmployeeID = e.EmployeeID
+--- then last_Value will be the current row
