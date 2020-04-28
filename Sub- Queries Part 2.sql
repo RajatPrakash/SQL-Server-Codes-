2982c264c636f3a5eb2c5b15fb056af209ec92b4
Rajat Prakash <rajatprakash904@gmail.com>
2020-04-28 12:37:47 +0530

    Working with where clause in sub-queries

---
+++ Sub- Queries Part 2.sql
@@ -0,0 +1,21 @@
+use [Rajat 720_140]
+go
+
+--- where Clause in sub queries-------
+
+select * from tblTransaction as t
+join (select * from EMPLOYEE 
+where EmployeeLastName like 'y%' ) as e
+on e.EmployeeID = t.EmployeeID
+order by t.EmployeeID
+----we can also get the same result with normal join query
+
+select * from tblTransaction as t
+join EMPLOYEE as e
+on e.EmployeeID = t.EmployeeID
+and e.EmployeeLastName like 'y%'
+order by t.EmployeeID
+
+
+--which is better? use display estimate execution plan button!
+
