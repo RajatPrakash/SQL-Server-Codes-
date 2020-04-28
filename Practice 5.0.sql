14c3f9cc3d6d26e84bffba4b6f9331f65df322ae
Rajat Prakash <rajatprakash904@gmail.com>
2020-04-28 12:46:29 +0530

    Working with Estimate Execution Plan, within 2 sub-Queries

---
+++ Practice 5.0.sql
@@ -0,0 +1,15 @@
+use [Rajat 720_140]
+go
+---Working with Estimate Execution Plan
+select * from tblTransaction as t
+join (select * from EMPLOYEE 
+where EmployeeLastName like 'y%' ) as e
+on e.EmployeeID = t.EmployeeID
+order by t.EmployeeID
+
+
+select * from tblTransaction as t
+left join EMPLOYEE as e
+on e.EmployeeID = t.EmployeeID
+where e.EmployeeLastName like 'y%'
+order by t.EmployeeID
