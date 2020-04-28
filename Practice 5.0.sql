bc08c5bee66a109aad6e927dc6ac75bd62bd3259
Rajat Prakash <rajatprakash904@gmail.com>
2020-04-28 13:31:41 +0530

    Working with where clause in sub-queries

--- Practice 5.0.sql
+++ Practice 5.0.sql
@@ -11,5 +11,11 @@ order by t.EmployeeID
 select * from tblTransaction as t
 left join EMPLOYEE as e
 on e.EmployeeID = t.EmployeeID
+and e.EmployeeLastName like 'y%'
+order by t.EmployeeID
+
+select * from tblTransaction as t
+left join EMPLOYEE as e
+on e.EmployeeID = t.EmployeeID
 where e.EmployeeLastName like 'y%'
 order by t.EmployeeID
