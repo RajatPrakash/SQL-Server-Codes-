c761b02c4715ec7a38d2f7f60e76e7dd86d3b488
Rajat Prakash <rajatprakash904@gmail.com>
2020-05-01 12:07:01 +0530

    Function can also be called with a variable , or with select query or with where also.

--- Functions.sql
+++ Functions.sql
@@ -1,2 +1,45 @@
 use [Rajat 720_140]
 go
+
+--Create a simple function to add 1 to amount in transaction table--
+
+--Creating a scaller function
+
+
+
+CREATE FUNCTION AmountPlusOne(@Amount Smallmoney)
+RETURNS smallmoney
+AS
+BEGIN
+	RETURN @Amount+1
+END
+GO
+
+
+select EMPLOYEEID,DateOfTransaction,Amount,dbo.AmountPlusOne(Amount) AS AMOUNTPLUSONE
+from tblTransaction
+
+---Function can also be called ---
+
+declare @Submission smallmoney
+exec @Submission = dbo.AmountPlusOne 223
+select @Submission
+go
+
+--or--
+declare @Submission smallmoney
+exec @Submission = dbo.AmountPlusOne @Amount = 223
+select @Submission
+
+
+
+
+
+
+
+
+
+
+
+
+
