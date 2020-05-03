58005318b090ca58bf0ee9af8fb7ecf53d86831d
Rajat Prakash <rajatprakash904@gmail.com>
2020-05-03 20:05:30 +0530

    Converting Data using XML resultset with AUTO

---
+++ Converting Table Data into AUTO XML_ResultSet.sql
@@ -0,0 +1,34 @@
+﻿<e EmployeeID="202" EmployeeFirstName="Merav" EmployeeLastName="Rizzi">
+  <t Amount="913.3600" DateOfTransaction="2015-04-12T00:00:00" />
+  <t Amount="-294.7200" DateOfTransaction="2015-01-11T00:00:00" />
+  <t Amount="-996.6900" DateOfTransaction="2015-10-17T00:00:00" />
+</e>
+<e EmployeeID="203" EmployeeFirstName="Reuben" EmployeeLastName="Richter">
+  <t Amount="-1570.8000" DateOfTransaction="2014-04-23T00:00:00" />
+  <t Amount="-614.2900" DateOfTransaction="2015-02-19T00:00:00" />
+  <t Amount="-1228.5800" DateOfTransaction="2015-02-20T00:00:00" />
+</e>
+<e EmployeeID="204" EmployeeFirstName="Eric" EmployeeLastName="Richmeier">
+  <t Amount="-582.6900" DateOfTransaction="2015-08-29T00:00:00" />
+  <t Amount="602.7800" DateOfTransaction="2015-01-06T00:00:00" />
+</e>
+<e EmployeeID="205" EmployeeFirstName="Sandeep" EmployeeLastName="Richins">
+  <t Amount="159.6100" DateOfTransaction="2014-09-05T00:00:00" />
+  <t Amount="-644.7600" DateOfTransaction="2014-09-10T00:00:00" />
+  <t Amount="-646.6100" DateOfTransaction="2015-12-15T00:00:00" />
+  <t Amount="2250.4200" DateOfTransaction="2014-07-05T00:00:00" />
+  <t Amount="1960.2300" DateOfTransaction="2015-11-01T00:00:00" />
+  <t Amount="-1293.2200" DateOfTransaction="2015-12-16T00:00:00" />
+</e>
+<e EmployeeID="206" EmployeeFirstName="Mihail" EmployeeLastName="Richins">
+  <t Amount="980.2500" DateOfTransaction="2014-09-12T00:00:00" />
+</e>
+<e EmployeeID="207" EmployeeFirstName="Jack" EmployeeLastName="Richards">
+  <t Amount="736.0200" DateOfTransaction="2014-08-02T00:00:00" />
+</e>
+<e EmployeeID="208" EmployeeFirstName="Patrick" EmployeeLastName="Richards">
+  <t Amount="-385.8300" DateOfTransaction="2015-01-05T00:00:00" />
+  <t Amount="-319.3300" DateOfTransaction="2014-11-16T00:00:00" />
+  <t Amount="-113.1900" DateOfTransaction="2015-10-20T00:00:00" />
+  <t Amount="-771.6600" DateOfTransaction="2015-01-06T00:00:00" />
+</e>
