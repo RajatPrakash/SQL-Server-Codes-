24efe92c4d606419988d431bdecec95d672413ca
Rajat Prakash <rajatprakash904@gmail.com>
2020-05-03 20:07:52 +0530

    Converting Data using XML resultset with RAWXML

---
+++ Converting Table Data into RawXML_ResultSet.sql
@@ -0,0 +1,140 @@
+﻿<MyRow>
+  <EmployeeID>202</EmployeeID>
+  <EmployeeFirstName>Merav</EmployeeFirstName>
+  <EmployeeLastName>Rizzi</EmployeeLastName>
+  <Amount>913.3600</Amount>
+  <DateOfTransaction>2015-04-12T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>202</EmployeeID>
+  <EmployeeFirstName>Merav</EmployeeFirstName>
+  <EmployeeLastName>Rizzi</EmployeeLastName>
+  <Amount>-294.7200</Amount>
+  <DateOfTransaction>2015-01-11T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>202</EmployeeID>
+  <EmployeeFirstName>Merav</EmployeeFirstName>
+  <EmployeeLastName>Rizzi</EmployeeLastName>
+  <Amount>-996.6900</Amount>
+  <DateOfTransaction>2015-10-17T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>203</EmployeeID>
+  <EmployeeFirstName>Reuben</EmployeeFirstName>
+  <EmployeeLastName>Richter</EmployeeLastName>
+  <Amount>-1570.8000</Amount>
+  <DateOfTransaction>2014-04-23T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>203</EmployeeID>
+  <EmployeeFirstName>Reuben</EmployeeFirstName>
+  <EmployeeLastName>Richter</EmployeeLastName>
+  <Amount>-614.2900</Amount>
+  <DateOfTransaction>2015-02-19T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>203</EmployeeID>
+  <EmployeeFirstName>Reuben</EmployeeFirstName>
+  <EmployeeLastName>Richter</EmployeeLastName>
+  <Amount>-1228.5800</Amount>
+  <DateOfTransaction>2015-02-20T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>204</EmployeeID>
+  <EmployeeFirstName>Eric</EmployeeFirstName>
+  <EmployeeLastName>Richmeier</EmployeeLastName>
+  <Amount>-582.6900</Amount>
+  <DateOfTransaction>2015-08-29T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>204</EmployeeID>
+  <EmployeeFirstName>Eric</EmployeeFirstName>
+  <EmployeeLastName>Richmeier</EmployeeLastName>
+  <Amount>602.7800</Amount>
+  <DateOfTransaction>2015-01-06T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>205</EmployeeID>
+  <EmployeeFirstName>Sandeep</EmployeeFirstName>
+  <EmployeeLastName>Richins</EmployeeLastName>
+  <Amount>159.6100</Amount>
+  <DateOfTransaction>2014-09-05T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>205</EmployeeID>
+  <EmployeeFirstName>Sandeep</EmployeeFirstName>
+  <EmployeeLastName>Richins</EmployeeLastName>
+  <Amount>-644.7600</Amount>
+  <DateOfTransaction>2014-09-10T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>205</EmployeeID>
+  <EmployeeFirstName>Sandeep</EmployeeFirstName>
+  <EmployeeLastName>Richins</EmployeeLastName>
+  <Amount>-646.6100</Amount>
+  <DateOfTransaction>2015-12-15T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>205</EmployeeID>
+  <EmployeeFirstName>Sandeep</EmployeeFirstName>
+  <EmployeeLastName>Richins</EmployeeLastName>
+  <Amount>2250.4200</Amount>
+  <DateOfTransaction>2014-07-05T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>205</EmployeeID>
+  <EmployeeFirstName>Sandeep</EmployeeFirstName>
+  <EmployeeLastName>Richins</EmployeeLastName>
+  <Amount>1960.2300</Amount>
+  <DateOfTransaction>2015-11-01T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>205</EmployeeID>
+  <EmployeeFirstName>Sandeep</EmployeeFirstName>
+  <EmployeeLastName>Richins</EmployeeLastName>
+  <Amount>-1293.2200</Amount>
+  <DateOfTransaction>2015-12-16T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>206</EmployeeID>
+  <EmployeeFirstName>Mihail</EmployeeFirstName>
+  <EmployeeLastName>Richins</EmployeeLastName>
+  <Amount>980.2500</Amount>
+  <DateOfTransaction>2014-09-12T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>207</EmployeeID>
+  <EmployeeFirstName>Jack</EmployeeFirstName>
+  <EmployeeLastName>Richards</EmployeeLastName>
+  <Amount>736.0200</Amount>
+  <DateOfTransaction>2014-08-02T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>208</EmployeeID>
+  <EmployeeFirstName>Patrick</EmployeeFirstName>
+  <EmployeeLastName>Richards</EmployeeLastName>
+  <Amount>-385.8300</Amount>
+  <DateOfTransaction>2015-01-05T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>208</EmployeeID>
+  <EmployeeFirstName>Patrick</EmployeeFirstName>
+  <EmployeeLastName>Richards</EmployeeLastName>
+  <Amount>-319.3300</Amount>
+  <DateOfTransaction>2014-11-16T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>208</EmployeeID>
+  <EmployeeFirstName>Patrick</EmployeeFirstName>
+  <EmployeeLastName>Richards</EmployeeLastName>
+  <Amount>-113.1900</Amount>
+  <DateOfTransaction>2015-10-20T00:00:00</DateOfTransaction>
+</MyRow>
+<MyRow>
+  <EmployeeID>208</EmployeeID>
+  <EmployeeFirstName>Patrick</EmployeeFirstName>
+  <EmployeeLastName>Richards</EmployeeLastName>
+  <Amount>-771.6600</Amount>
+  <DateOfTransaction>2015-01-06T00:00:00</DateOfTransaction>
+</MyRow>
