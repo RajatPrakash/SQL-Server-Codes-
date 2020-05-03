5e5b9ed749c7455fe84f7791cf4b369a4d4d33ed
Rajat Prakash <rajatprakash904@gmail.com>
2020-05-03 20:00:50 +0530

    Modifying XML data with result set

---
+++ Modify XML Values_Resultset.sql
@@ -0,0 +1,13 @@
+﻿<Shopping ShopperName="Rajat Prakash" Weather="Nice">
+  <ShoppingTrip ShoppingTripID="L1">
+    <Item Cost="5">Bananas</Item>
+    <Item Cost="5">Apples</Item>
+    <Item Cost="5">Cherries</Item>
+  </ShoppingTrip>
+  <ShoppingTrip ShoppingTripID="L2">
+    <Item>Emeralds</Item>
+    <Item>Diamonds</Item>
+    <Item>Furniture</Item>
+    <Item>"platinum"</Item>
+  </ShoppingTrip>
+</Shopping>
