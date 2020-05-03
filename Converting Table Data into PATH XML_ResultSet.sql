6b9828b63e3ebd43a1a916903975ae868ccf04a5
Rajat Prakash <rajatprakash904@gmail.com>
2020-05-03 20:06:25 +0530

    Converting Data using XML resultset with PATH

---
+++ Converting Table Data into PATH XML_ResultSet.sql
@@ -0,0 +1,55 @@
+﻿<row FirstName="Merav" lastName="Rizzi">
+  <EmployeeID>202</EmployeeID>
+  <Transaction>
+    <amount>913.3600</amount>
+    <amount>-294.7200</amount>
+    <amount>-996.6900</amount>
+  </Transaction>
+</row>
+<row FirstName="Reuben" lastName="Richter">
+  <EmployeeID>203</EmployeeID>
+  <Transaction>
+    <amount>-1570.8000</amount>
+    <amount>-614.2900</amount>
+    <amount>-1228.5800</amount>
+  </Transaction>
+</row>
+<row FirstName="Eric" lastName="Richmeier">
+  <EmployeeID>204</EmployeeID>
+  <Transaction>
+    <amount>-582.6900</amount>
+    <amount>602.7800</amount>
+  </Transaction>
+</row>
+<row FirstName="Sandeep" lastName="Richins">
+  <EmployeeID>205</EmployeeID>
+  <Transaction>
+    <amount>159.6100</amount>
+    <amount>-644.7600</amount>
+    <amount>-646.6100</amount>
+    <amount>2250.4200</amount>
+    <amount>1960.2300</amount>
+    <amount>-1293.2200</amount>
+  </Transaction>
+</row>
+<row FirstName="Mihail" lastName="Richins">
+  <EmployeeID>206</EmployeeID>
+  <Transaction>
+    <amount>980.2500</amount>
+  </Transaction>
+</row>
+<row FirstName="Jack" lastName="Richards">
+  <EmployeeID>207</EmployeeID>
+  <Transaction>
+    <amount>736.0200</amount>
+  </Transaction>
+</row>
+<row FirstName="Patrick" lastName="Richards">
+  <EmployeeID>208</EmployeeID>
+  <Transaction>
+    <amount>-385.8300</amount>
+    <amount>-319.3300</amount>
+    <amount>-113.1900</amount>
+    <amount>-771.6600</amount>
+  </Transaction>
+</row>
