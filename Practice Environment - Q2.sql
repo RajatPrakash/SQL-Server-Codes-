5f5bb0aea1c967e8bfec42090a646eb756c85c8c
Rajat Prakash <rajatprakash904@gmail.com>
2020-05-05 20:26:40 +0530

    Practicing More Basic join queries

--- Practice Environment - Q2.sql
+++ Practice Environment - Q2.sql
@@ -17,3 +17,14 @@ select ci.city_name,c.cname from country c join state s
 on s.cid = c.cid join city ci on
 ci.sid = s.sid
 order by c.cname
+
+-- Query to find out states that are in USA
+
+select S.sname,c.cname from country c join state s 
+on c.cid =s.cid
+where c.cname = 'USA'
+
+-- Query to find out in which state particular city is located
+select s.sname,c.city_name from  city c join state s 
+on s.sid = c.sid
+order by s.sname
