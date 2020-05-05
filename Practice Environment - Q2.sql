be635f4fc25546388941349a3218ba40ccedbc85
Rajat Prakash <rajatprakash904@gmail.com>
2020-05-05 19:52:49 +0530

    Practicing Basic join queries

---
+++ Practice Environment - Q2.sql
@@ -0,0 +1,19 @@
+Use rajat_practice
+
+
+select * from country
+select * from city
+select * from state
+
+-- query to find Country with respective states
+
+select s.sname,c.cname from State s join country c 
+on 
+s.cid= c.cid
+
+-- query to find Country with respective city
+
+select ci.city_name,c.cname from country c join state s 
+on s.cid = c.cid join city ci on
+ci.sid = s.sid
+order by c.cname
